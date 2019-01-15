9f20e0d03dfa0df23529f3a399204b0d017151b2
kmietotwy <kmietotwy@gmail.com>
2019-01-14 18:56:37 -1000

    completed exercises

--- exercise.js
+++ exercise.js
@@ -13,7 +13,8 @@
 /*1. Mr. Bonner (aka the Red Rocket, the Red Mamba) doesn't belong in this class of Hall Of Famers. Remove this imposter immediately and assign it to a new variable named `notHof` Console.log this new variable as well as the original list.*/ 
 
 var hof = ["Jordan", "Bird", "Kobe", "Duncan", "Iverson", "Bonner"];
-
+hof.pop()
+console.log(hof);
 
 
 /* The array.push() adds one or more element to the end of an array and returns the new length of the array*/
@@ -21,7 +22,8 @@ var hof = ["Jordan", "Bird", "Kobe", "Duncan", "Iverson", "Bonner"];
 /*2. You've been tasked to do this week's grocery shopping. As you arrive at Times Supermarket, Kanye pings you to get a bottle of Hendricks gin. Add this to the existing shoppingList and console.log it.*/
 
 var shoppingList = ["cool ranch doritos", "kings hawaiian sweet bread", "peanut butter oreos", "fruit loops cereal"];
-
+shoppingList.push('Hendricks gin');
+console.log(shoppingList);
 
 
 
@@ -30,7 +32,8 @@ var shoppingList = ["cool ranch doritos", "kings hawaiian sweet bread", "peanut 
 /*3. Use the force, or in this case the reverse method to help Yoda make some sense with his motivational talk to the young Jedi interns.*/
 
 var yoda = ["try", "no", "is", "there", "not", "do", "or", "do"];
-
+yoda.reverse();
+console.log(yoda);
 
 
 /*The array.shift() removes the first element from an array and returns that element*/
@@ -38,13 +41,16 @@ var yoda = ["try", "no", "is", "there", "not", "do", "or", "do"];
 /*4. People been lining up for hours to get the newest iphone release. Help manage the unruly crowd of privileged customers by serving them one at a time and assiging it to a variable named `nowServing`. Console.log this new variable as well as the waitList.*/
 
 var waitList = ["Chance the Rapper", "Khalid", "Tay-Tay", "Barry Manilow", "Piko Taro"];
-
+var nowServing = waitList.shift();
+console.log(nowServing);
 
 
 /*The array.unshift() adds one or more elements to the front of an array and returns the new length of the array*/
 
 /*5. Apparently, you didn't do a good job with crowd control in the previous exercise. Kanye just cut in front of everyone!. Add Kanye to the waitList array above. Console.log waitList*/
 
+waitList.unshift('Kanye');
+console.log(waitList);
 
 
 /*The array.splice() adds and/or removes elements from an array.
@@ -54,6 +60,8 @@ array.splice(index, how many items to be removed)*/
 
 var testMenu = ["McPizza Burger", "McFly Burger", "McLinguine", "McChar-Siu Bau", "McChop-Chae", "McPancit"];
 
+testMenu.splice(2, 2);
+console.log(testMenu);
 
 
 /*=============================================================*/
@@ -66,6 +74,8 @@ var testMenu = ["McPizza Burger", "McFly Burger", "McLinguine", "McChar-Siu Bau"
 
 var westSide = ["Dre", "2Pac", "Snoop"];
 var eastSide = ["Biggie", "Diddy", "Craig Mack"];
+var oneLove = westSide.concat(eastSide)
+console.log(oneLove);
 
 
 
@@ -74,6 +84,9 @@ var eastSide = ["Biggie", "Diddy", "Craig Mack"];
 /*8. Help Nike sell more overpriced sneakers by changing their slogan from an array into a string. Convert the shoe array and assign it to a variable named `shoeString`. Console.log this new variable.*/
 
 var shoe = ["just", "do", "it"];
+var shoeString = shoe.join(' ');
+console.log(shoeString);
+
 
 
 
@@ -86,6 +99,7 @@ array.slice(index start, index end)*/
 var fbFriends = ["Khalid", "Yeezy", "Kimmy K", "Lamar Odom", "Lebron", "Warren Buffet"];
 
 
-
+var unFriend = fbFriends.slice(1, 3);
+console.log(unFriend);
 
 
